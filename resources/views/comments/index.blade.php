f961a120010d65ab8af9150f2ef4ece231b7d856
jinyoung <siaert000@gmail.com>
2017-09-15 18:04:06 +0900

    댓글기능 fix #1

--- resources/views/comments/index.blade.php
+++ resources/views/comments/index.blade.php
@@ -20,3 +20,28 @@
         @empty
     @endforelse
 </div>
+
+
+@section('script')
+    @parent
+    <script>
+        // 댓글 삭제 요청을 처리한다.
+        $('.btn__delete__comment').on('click', function (e) {
+            var commentId = $(this).closest('.item__comment').data('id'),
+                articleId = $('#item__article').data('id');
+            if (confirm('댓글을 삭제합니까?')) {
+                $.ajax({
+                    type: 'DELETE',
+                    url: "/comments/" + commentId,
+                    data: {
+                        _method: "DELETE",
+                    }
+                }).then(function () {
+                    $('#comment_' + commentId).fadeOut(1000, function () {
+                        $(this).remove();
+                    });
+                });
+            }
+        });
+    </script>
+@stop
